a7373763643b6a5544c5ed81776da35435b7458b
Anderson Mosquera <anderson.mosquera9610@gmail.com>
2019-01-03 21:04:44 -0500

    add express on project for deploy to Heroku

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+
+//Config
+app.set('port', process.env.PORT || 3000);
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+//Server
+app.listen(app.get('port'), () => {
+   console.log(`Listening on port ${app.get('port')}`);
+});
+
+app.get('/', (req, res) => {
+   res.render('index');   
+});
---
+++ public/js/helpers.js
@@ -0,0 +1,32 @@
+let getRandomNumber =  (size) => {
+   rand = Math.random() * size;
+   return Math.floor(rand);
+}
+
+let getDistance = (e, target) => {
+   let diffX = e.offsetX - target.x;
+   let diffY = e.offsetY - target.y;
+   return Math.sqrt(diffX ** 2 + diffY ** 2);
+}
+
+let getDistanceHint = (distance) => {
+   if (distance < 30){
+      return "Hirbiendo"
+   }else if(distance < 40){
+      return "Muy Caliente"
+   }else if(distance < 60){
+      return "Cálido"
+   }else if(distance < 100){
+      return "Tibio"
+   }else if(distance < 120){
+      return "Templado";
+   }else if(distance < 160){
+      return "Frío";
+   }else if(distance < 240){
+      return "Muy frío";
+   }else{
+      return "Congelado"
+   }
+}
+
+   
